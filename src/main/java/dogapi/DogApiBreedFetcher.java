731b7a31ce4422d1d231ef4d2d8a7fabc792c647
CarlGergi <carl.gergi@mail.utoronto.ca>
2025-10-12 14:37:03 -0400

    fixed tasks 1 to 4

--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -24,12 +24,36 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // TODO Task 1: Complete this method based on its provided documentation
-        //      and the documentation for the dog.ceo API. You may find it helpful
-        //      to refer to the examples of using OkHttpClient from the last lab,
-        //      as well as the code for parsing JSON responses.
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException  {
+        if (breed == null || breed.trim().isEmpty()) {
+            throw new BreedNotFoundException("Breed must be non-empty");
+        }
+
+        String url = "https://dog.ceo/api/breed/" + breed.trim().toLowerCase() + "/list";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String jsonText = response.body().string();
+            JSONObject json = new JSONObject(jsonText);
+
+            if (json.has("status") && json.getString("status").equals("error")) {
+                throw new BreedNotFoundException("Breed not found");
+            }
+
+            JSONArray arr = json.getJSONArray("message");
+            List<String> subBreeds = new ArrayList<>();
+            for (int i = 0; i < arr.length(); i++) {
+                subBreeds.add(arr.getString(i));
+            }
+            return subBreeds;
+
+        } catch (IOException e) {
+            throw new BreedNotFoundException("Failed to connect to API");
+        } catch (Exception e) {
+            throw new BreedNotFoundException("Error fetching data");
+        }
     }
 }
